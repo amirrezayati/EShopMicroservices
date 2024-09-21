5c59d38844f49f5eccd67958978417c5a82f9edb
Amir Rezayati <Amir@DESKTOP-GMV5Q12>
2024-09-21 15:58:26 +0330

    Create building block project  and add cqrs to it and refrence to Catalog microservice

---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductCommandHandler.cs
@@ -0,0 +1,17 @@
+﻿using BuildingBlocks.CQRS;
+using MediatR;
+
+namespace Catalog.API.Products.CreateProduct
+{
+    internal class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
+    {
+        public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+        {
+            //Business logic to create a product
+            throw new NotImplementedException();
+        }
+    }
+
+    public record CreateProductCommand(string Name, string Description, string ImageFile, decimal Price, List<string> Categories) : ICommand<CreateProductResult>;
+    public record CreateProductResult(Guid Id);
+}
