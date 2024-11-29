47fee21eb4b35fba4e5cc56b8236f02bba3677d6
Amir Rezayati <Amir@DESKTOP-GMV5Q12>
2024-11-30 00:48:12 +0330

    Get Product by Id

---
+++ src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdEndPoint.cs
@@ -0,0 +1,25 @@
+﻿using Catalog.API.Products.GetProducts;
+
+namespace Catalog.API.Products.GetProductById
+{
+    public class GetProductByIdEndPoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapGet("/products/{id}", async(Guid id, ISender sender) =>
+            {
+                var result = await sender.Send(new GetProductByIdQuery(id));
+
+                var response = result.Adapt<GetProductByIdResponse>();
+                return Results.Ok(response);
+            })
+            .WithName("GetProductById")
+            .Produces<GetProductsResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Get Product By Id Summary")
+            .WithDescription("Get Product By Id Description");
+        }
+    }
+
+    public record GetProductByIdResponse(Product Product);
+}
---
+++ src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdQueryHandler.cs
@@ -0,0 +1,21 @@
+﻿using Catalog.API.Exceptions;
+using Catalog.API.Products.GetProducts;
+
+namespace Catalog.API.Products.GetProductById
+{
+    public class GetProductByIdQueryHandler(
+        IDocumentSession _documentSession,
+        ILogger<GetProductByIdQueryHandler> _logger) : IQueryHandler<GetProductByIdQuery, GetProductByIdResult>
+    {
+        public async Task<GetProductByIdResult> Handle(GetProductByIdQuery query, CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("GetProductByIdQueryHandler.Handle called with {@Query}", query);
+
+            var product = await _documentSession.LoadAsync<Product>(query.Id ,cancellationToken);
+            return product is null ? throw new ProductNotFoundException() : new GetProductByIdResult(product);
+        }
+    }
+
+    public record GetProductByIdQuery(Guid Id) : IQuery<GetProductByIdResult>;
+    public record GetProductByIdResult(Product Product);
+}
