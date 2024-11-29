cf7233381c07a8604a98c4d5c9c00347f376341c
Amir Rezayati <Amir@DESKTOP-GMV5Q12>
2024-11-29 23:48:56 +0330

    Add GetProducts and Add DockerCompose

---
+++ src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsQueryHandler.cs
@@ -0,0 +1,20 @@
+﻿using Marten.Linq.QueryHandlers;
+
+namespace Catalog.API.Products.GetProducts
+{
+    internal class GetProductsQueryHandler(
+        IDocumentSession _documentSession,
+        ILogger<GetProductsQueryHandler> _logger) : IQueryHandler<GetProductsQuery, GetProductsResult>
+    {
+        public async Task<GetProductsResult> Handle(GetProductsQuery query, CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("GetProductsQueryHandler.Handle called with {@Query}", query);
+
+            var products = await _documentSession.Query<Product>().ToListAsync(cancellationToken);
+            return new GetProductsResult(products);
+        }
+    }
+
+    public record GetProductsQuery : IQuery<GetProductsResult>;
+    public record GetProductsResult(IEnumerable<Product> Products);
+}
